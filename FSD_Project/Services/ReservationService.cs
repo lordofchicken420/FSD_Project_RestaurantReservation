7936a7be1b545ea0ddd0266d096e4313e53694a5
RUBEN JAU QUAN KANG 2301874J <2301874J@student.tp.edu.sg>
2025-01-10 16:45:08 +0800

    Changed datetime of reservations to datetime-local Added C# in reservations
    Small change but im committing now cos its terrifying lol

---
+++ FSD_Project/Services/ReservationService.cs
@@ -0,0 +1,73 @@
+﻿using FSD_Project.Data;
+using FSD_Project.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace FSD_Project.Services
+{
+    public interface IReservationService
+    {
+        Task<bool> CreateReservationAsync(Reservation reservation);
+        Task<List<Reservation>> GetReservationsAsync();
+        Task<bool> IsTableAvailableAsync(int tableId, DateTime reservationTime);
+    }
+
+    public class ReservationService : IReservationService
+    {
+        private readonly IDbContextFactory<FSD_ProjectContext> _contextFactory;
+
+        public ReservationService(IDbContextFactory<FSD_ProjectContext> contextFactory)
+        {
+            _contextFactory = contextFactory;
+        }
+
+        public async Task<bool> CreateReservationAsync(Reservation reservation)
+        {
+            try
+            {
+                using var context = await _contextFactory.CreateDbContextAsync();
+
+                // Check if table is available
+                if (!await IsTableAvailableAsync(reservation.TableID, reservation.ReservedDateTime))
+                {
+                    return false;
+                }
+
+                // Set audit fields
+                reservation.DateCreated = DateTime.Now;
+                reservation.DateUpdated = DateTime.Now;
+                reservation.CreatedBy = "System"; // You might want to get this from the logged-in user
+                reservation.UpdatedBy = "System";
+
+                context.Reservation.Add(reservation);
+                await context.SaveChangesAsync();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public async Task<List<Reservation>> GetReservationsAsync()
+        {
+            using var context = await _contextFactory.CreateDbContextAsync();
+            return await context.Reservation
+                .OrderByDescending(r => r.ReservedDateTime)
+                .ToListAsync();
+        }
+
+        public async Task<bool> IsTableAvailableAsync(int tableId, DateTime reservationTime)
+        {
+            using var context = await _contextFactory.CreateDbContextAsync();
+
+            // Check if there are any overlapping reservations
+            // Assuming a 2-hour dining duration
+            var endTime = reservationTime.AddHours(2);
+
+            return !await context.Reservation.AnyAsync(r =>
+                r.TableID == tableId &&
+                r.ReservedDateTime < endTime &&
+                r.ReservedDateTime.AddHours(2) > reservationTime);
+        }
+    }
+}
